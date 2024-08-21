62d8c71a66452ff65377ddc0ecdc886e181cdf84
Leroux63 <dleroux63160@gmail.com>
2024-08-21 10:32:18 +0200

    make soustraction , multiply and divide methods , tests OK

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -17,11 +17,29 @@ pub mod calculator {
         calculator.result = num1 + num2;
         Ok(())
     }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> Result<()> {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>,num1: i64, num2: i64)->Result<()>{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>,num1: i64, num2: i64)->Result<()>{
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
 pub struct Create<'info> {
-    #[account(init, payer = user, space = 8 + 64 + 8)] // `space` ajusté pour la taille du compte
+    #[account(init, payer = user, space = 8 + 64 + 8)] // Ajuster en fonction des besoins
     pub calculator: Account<'info, Calculator>,
     
     #[account(mut)] // Le compte payeur doit être mutable
@@ -35,6 +53,23 @@ pub struct Addition<'info> {
     pub calculator: Account<'info, Calculator>,
 }
 
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info,Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Division<'info>{
+    #[account(mut)]
+    pub calculator: Account<'info,Calculator>,
+}
 #[account]
 pub struct Calculator {
     pub greeting: String,
--- tests/calculator.ts
+++ tests/calculator.ts
@@ -32,4 +32,32 @@ describe("calculator", () => {
     const account = await program.account.calculator.fetch(calculatorPair.publicKey);
     expect(account.result.toString()).to.eql(new anchor.BN(5).toString());
   });
+
+  it("Subtraction",async()=>{
+    await program.methods.subtract(new anchor.BN(5),new anchor.BN(2))
+      .accounts({
+        calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result.toString()).to.eql(new anchor.BN(3).toString());
+  });
+  it("Multiplication",async()=>{
+    await program.methods.multiply(new anchor.BN(8),new anchor.BN(7))
+      .accounts({
+        calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result.toString()).to.eql(new anchor.BN(56).toString());
+  });
+  it("Division",async()=>{
+    await program.methods.divide(new anchor.BN(14),new anchor.BN(2))
+      .accounts({
+        calculator: calculatorPair.publicKey,
+      })
+      .rpc();
+    const account = await program.account.calculator.fetch(calculatorPair.publicKey);
+    expect(account.result.toString()).to.eql(new anchor.BN(7).toString());
+  });
 });
